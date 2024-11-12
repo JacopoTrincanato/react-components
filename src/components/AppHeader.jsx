3a10da1164339fb6f5d3f5aff943b8340e74abf9
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-12 15:26:53 +0100

    create header component

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+//creo il componente header
+export default function AppHeader() {
+    //creo una variabile dove inserire il titolo
+    const title = 'Il mio blog';
+
+    //eseguo il return
+    return (
+        <h1>{title}</h1>
+    )
+}
