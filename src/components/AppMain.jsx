606db0496794d3bed002d93bcb6a6590f3e94170
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-12 15:54:22 +0100

    create card markup inside AppMain component

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -9,10 +9,23 @@ export default function AppMain() {
     //creo una variabile per la descrizione della card
     const cardDescription = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum magnam autem consectetur pariatur reiciendis a, vero voluptate ad modi earum et nisi quia possimus, numquam adipisci magni delectus quasi ab.';
 
+    //creo una variabile per il testo del bottone
+    const buttontext = 'leggi di più';
+
     //eseguo il return
     return (
         <main>
+            <div className="card">
+                <div className="card-top">
+                    <p>{cardSize}</p>
+                </div>
 
+                <div className="card-bottom">
+                    <h3>{cardTitle}</h3>
+                    <p>{cardDescription}</p>
+                    <button className="btn">{buttontext.toUpperCase()}</button>
+                </div>
+            </div>
         </main>
 
     )
