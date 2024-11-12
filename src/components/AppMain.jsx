57de814063227d524d57e7d321cdd13c28b6fe2b
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-12 15:48:42 +0100

    create cardSize, CardTitle and cardDescription in AppMain component

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,6 +1,13 @@
 //creo il componente main
 export default function AppMain() {
+    //creo una variabile per il testo che indica la dimensione della card
+    const cardSize = '600x400';
 
+    //creo una variabile per il titolo della card
+    const cardTitle = 'Titolo del post';
+
+    //creo una variabile per la descrizione della card
+    const cardDescription = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum magnam autem consectetur pariatur reiciendis a, vero voluptate ad modi earum et nisi quia possimus, numquam adipisci magni delectus quasi ab.';
 
     //eseguo il return
     return (
